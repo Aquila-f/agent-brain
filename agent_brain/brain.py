abcd38644d22497536a07affbd240d94c9feba06
Aquila-f <aquila@AquiladeMacBook-Air.local>
2025-09-27 12:52:36 +0800

    feat: set net and memory from outside

--- agent_brain/brain.py
+++ agent_brain/brain.py
@@ -1,9 +1,59 @@
 import asyncio
 from collections.abc import AsyncIterator
+from enum import Enum
 
-from agent_brain.memory import MessagesMemory
+from agent_brain.memory import Memory, MessagesMemory
 from agent_brain.thinking_net import create_react_net
 from agent_brain.tool import BaseTool
+
+from .thinking_net import State
+
+NET_MAP = {"ReAct": create_react_net()}
+MEM_MAP = {"Messages": MessagesMemory}
+
+
+class Brain:
+    def __init__(
+        self,
+        thinking_net: str | dict[Enum, State],
+        memory_struct: str | Memory,
+        tools: list[BaseTool],
+    ) -> None:
+        self.net = self._create_net(thinking_net)
+        self.memory = self._create_memory(memory_struct, tools)
+        self.state = list(self.net.values())[0]
+        self.loop_limit = 10
+
+    async def _step(self) -> AsyncIterator[str]:
+        self.state.on_enter(self.memory)
+        async for chunk in self.state.run(self.memory):
+            yield chunk
+        self.state.on_exit(self.memory)
+
+        next_state_enum = await self.state.next_state(self.memory)
+        self.state = self.net[next_state_enum]
+
+    async def answer(self, task: str) -> AsyncIterator[str]:
+        await self.memory.set_goal(task)
+
+        while not self.memory.done and self.loop_limit > 0:
+            async for chunk in self._step():
+                yield chunk
+            self.loop_limit -= 1
+
+    def _create_net(self, thinking_net: str | dict[Enum, State]) -> dict[Enum, State]:
+        if isinstance(thinking_net, str):
+            if thinking_net not in NET_MAP:
+                raise ValueError(f"Unknown thinking net: {thinking_net}")
+            return NET_MAP[thinking_net]
+        return thinking_net
+
+    def _create_memory(self, memory_struct: str | Memory, tools: list[BaseTool]) -> Memory:
+        if isinstance(memory_struct, str):
+            if memory_struct not in MEM_MAP:
+                raise ValueError(f"Unknown memory struct: {memory_struct}")
+            return MEM_MAP[memory_struct](tools=tools)
+        return memory_struct
 
 
 class AddTwoNumbersTool(BaseTool):
@@ -27,33 +77,8 @@ class AddTwoNumbersTool(BaseTool):
         return f"tool result: {a} + {b} = {a + b}"
 
 
-class Brain:
-    def __init__(self, tools: list[BaseTool]) -> None:
-        self.thinking_net = create_react_net()
-        self.state = list(self.thinking_net.values())[0]
-        self.memory = MessagesMemory(tools=tools)
-        self.loop_limit = 10
-
-    async def _step(self) -> AsyncIterator[str]:
-        self.state.on_enter(self.memory)
-        async for chunk in self.state.run(self.memory):
-            yield chunk
-        self.state.on_exit(self.memory)
-
-        next_state_enum = await self.state.next_state(self.memory)
-        self.state = self.thinking_net[next_state_enum]
-
-    async def answer(self, task: str) -> AsyncIterator[str]:
-        await self.memory.set_goal(task)
-
-        while not self.memory.done and self.loop_limit > 0:
-            async for chunk in self._step():
-                yield chunk
-            self.loop_limit -= 1
-
-
 if __name__ == "__main__":
-    brain = Brain(tools=[AddTwoNumbersTool()])
+    brain = Brain(thinking_net="ReAct", memory_struct="Messages", tools=[AddTwoNumbersTool()])
     task = "What is the value of 12345 + 67890 + 999999?"
 
     async def main() -> None:
